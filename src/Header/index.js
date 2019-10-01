8ca208e5c77c0ba911c5433b48dc87ad32602b6a
Jeremy Chon <jaedee95@gmail.com>
2019-10-01 12:09:41 -0500

    added onclick function

--- src/Header/index.js
+++ src/Header/index.js
@@ -52,20 +52,24 @@ class Header extends React.Component {
 					<Icon 
 						name='user circle'
 						className='nav-profile'
+						onClick={this.goToProfile}
 					/>
 					<div 
 						className='nav-button'
 						id='nav-home'
+						onClick={this.goHome}
 					>HOME</div>
 					<div 
 						className='nav-button' 
 						id='nav-ind'
+						onClick={this.goToCategories}
 					>INDUSTRIES</div>
 				</div>
 				<div className='nav-title'>PAINPOINTS</div>
 				<div 
 					className='nav-button'
 					id='nav-logout'
+					onClick={this.handleLogout}
 				>LOGOUT</div>
 			</div>
 		)
