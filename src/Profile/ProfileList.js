abdc7ee6055be6fa12f09691e8199aec5a879149
Jeremy Chon <jaedee95@gmail.com>
2019-10-02 02:10:46 -0500

    updated css to profile page

--- src/Profile/ProfileList.js
+++ src/Profile/ProfileList.js
@@ -1,41 +1,59 @@
 import React from 'react'
-import './profile.css'
 import { Grid, Container, List } from 'semantic-ui-react'
 
 const ProfileList = (props) => {
-	console.log(props, '<--- props in ProfileList');
 
 	const userPainpoints = props.painpoints.map((pp, i) => {
 		return (
-			<div key={'pp' + i} style={{display: 'inline-block', width: '50%', textAlign: 'center'}}>
-				<div>Head: {pp.head}</div>
-				<div>Body: {pp.body}</div>
-				<div>Attachment: {pp.attachment}</div><br />
+			<div 
+				key={'pp' + i}
+				className='profile-item'
+				id='profile-pp-item'
+			>
+				<div style={{width: '100%'}}>Head: {pp.head}</div>
+				<div style={{width: '100%'}}>Body: {pp.body}</div>
+				<div style={{width: '100%'}}>Attachment: {pp.attachment}</div>
 			</div>
 		)
 	})
 
 	const userSolutions = props.solutions.map((sol, i) => {
 		return (
-			<div key={'sol' + i} style={{display: 'inline-block', width: '50%', textAlign: 'center'}}>
-				<div>Head: {sol.head}</div>
-				<div>Body: {sol.body}</div>
-				<div>Attachment: {sol.attachment}</div><br />
+			<div 
+				key={'sol' + i} 
+				className='profile-item'
+				id='profile-sol-item'
+			>
+				<div style={{width: '100%'}}>Head: {sol.head}</div>
+				<div style={{width: '100%'}}>Body: {sol.body}</div>
+				<div style={{width: '100%'}}>Attachment: {sol.attachment}</div>
 			</div>
 		)
 	})
 
 	return (
-		<Grid columns={2} divided>
-			<Grid.Column style={{padding: 0, textAlign: 'center'}}>
-				<Container className='list-titles' style={{backgroundColor: '#4f5fd1'}}>Your Painpoints</Container>
-				<List className='user-list' style={{display: 'inline-block', top: '20', width: '50%', padding: 0, listStyleType: 'none', textAlign: 'center'}}>{userPainpoints}</List>
-			</Grid.Column>
-			<Grid.Column style={{padding: 0, textAlign: 'center'}}>
-				<Container className='list-titles' style={{backgroundColor: '#62806A'}}>Your Solutions</Container>
-				<List className='user-list' style={{display: 'inline-block', top: '20', width: '50%', padding: 0, listStyleType: 'none', textAlign: 'center'}}>{userSolutions}</List>
-			</Grid.Column>
-		</Grid>
+		<div className='profile-list'>
+			<div 
+				className='profile-pp-sol'
+				id='profile-painpoint'
+			>
+				<div 
+					className='profile-title'
+					id='profile-title-painpoint'
+				>YOUR PAINPOINTS</div>
+				<div>{userPainpoints}</div>
+			</div>
+			<div 
+				className='profile-pp-sol'
+				id='profile-solution'
+			>
+				<div 
+					className='profile-title'
+					id='profile-title-solution'
+				>YOUR SOLUTIONS</div>
+				<div>{userSolutions}</div>
+			</div>
+		</div>
 	)
 }
 
--- src/Profile/index.js
+++ src/Profile/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ProfileList from '../Profile/ProfileList'
 import { Divider } from 'semantic-ui-react'
-import './profile.css'
 
 class Profile extends React.Component {
 	constructor() {
@@ -13,7 +12,6 @@ class Profile extends React.Component {
 			id: '',
 			painpoints: [],
 			solutions: []
-			// profile picture
 		}
 	}
 
@@ -22,41 +20,36 @@ class Profile extends React.Component {
 	}
 
 	getUserInfo = async () => {
-    try {
-      const userInfoResponse = await fetch('http://localhost:8000/user/' + this.props.match.params.id, {
-        method: 'GET',
-        credentials: 'include'
-      })
-      console.log(userInfoResponse, '<--- userInfoResponse');
+	    try {
+	      const userInfoResponse = await fetch('http://localhost:8000/user/' + this.props.match.params.id, {
+	        method: 'GET',
+	        credentials: 'include'
+	      })
 
-      if (userInfoResponse.status !== 200) {
-        throw Error('userInfoResponse does not work')
-      }
+	      if (userInfoResponse.status !== 200) {
+	        throw Error('userInfoResponse does not work')
+	      }
 
-      const parsedUserInfo = await userInfoResponse.json();
-      console.log(parsedUserInfo, '<--- parsedUserInfo');
+	      const parsedUserInfo = await userInfoResponse.json();
 
-      if (parsedUserInfo) {
-        this.setState({
-          ...parsedUserInfo.data,
-          username: parsedUserInfo.data.user.username,
-          fullName: parsedUserInfo.data.user.full_name,
-          id: parsedUserInfo.data.user.id
-        })
-        return parsedUserInfo.data
-      }
+	      if (parsedUserInfo) {
+	        this.setState({
+	          ...parsedUserInfo.data,
+	          username: parsedUserInfo.data.user.username,
+	          fullName: parsedUserInfo.data.user.full_name,
+	          id: parsedUserInfo.data.user.id
+	        })
+	        return parsedUserInfo.data
+	      }
 
-    } catch (err) {
-      console.log(err);
-    }
-  }
+	    } catch (err) {
+	      console.log(err);
+	    }
+	  }
 
 	render() {
-		console.log(this.state, '<--- state in profile');
 		return(
-			<div>
-				<h1 style={{textAlign: 'center', margin: '5%', color: '#279259'}}>User Profile Page</h1>
-				<Divider />
+			<div className='profile'>
 				<ProfileList painpoints={this.state.painpoints} solutions={this.state.solutions}/>
 			</div>
 		)
